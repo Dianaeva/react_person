a4b34975a0294fa7a721c03cca70d4eb8ba037f2
Diana Yevstignyeyeva <diana.yevstignyeyeva@gmail.com>
2026-03-31 11:45:53 +0300

    fix the age check

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,9 @@ import './Person.scss';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
+    {person.age !== undefined && (
+      <p className="Person__age">I am {person.age}</p>
+    )}
     <p className="Person__partner">
       {person.isMarried &&
         person.sex === 'm' &&
